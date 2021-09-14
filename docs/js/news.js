1d5864fe79a0a4a91408a66437856eaac80cdcc3
H37kouya <h37kouya@gmail.com>
2021-09-14 19:24:58 +0900

    feature newsListの追加

---
+++ docs/js/news.js
@@ -0,0 +1,14 @@
+// プログラムによる自動生成です。
+
+window.newsList = [
+    {
+        "title": "News1",
+        "imageLink": "/top_picture.png",
+        "createdAt": "2020-09-01",
+    },
+    {
+        "title": "News2",
+        "imageLink": "/top_picture.png",
+        "createdAt": "2020-09-02",
+    },
+]
